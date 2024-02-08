ffd28e171560f5b653adab3ff5356703c3938d80
zy <3325246991@qq.com>
2024-02-08 16:26:36 +0800

    continue

--- src/component/Comics/index.tsx
+++ src/component/Comics/index.tsx
@@ -1,22 +1,68 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { memo, useState } from 'react'
-import type { FC, ReactNode } from 'react'
-import ComicsWrapper from './styled'
+import React, { memo, useState } from "react";
+import type { FC, ReactNode } from "react";
+import ComicsWrapper from "./styled";
+import SvgIcon from "../SvgIcon";
+import { Swiper } from "antd-mobile";
 
 interface IProps {
-    children?: ReactNode
+  children?: ReactNode;
 }
 
 const Comics: FC<IProps> = () => {
-    const [isCartoon, SetIsCarToon] = useState(true)
+  const [isCartoon, SetIsCarToon] = useState(1);
+  function ClicikType(index: number) {
+    SetIsCarToon(index);
+  }
+  const colors = ["#ace0ff", "#bcffbd", "#e4fabd", "#ffcfac"];
+  return (
+    <ComicsWrapper>
+      {/* 漫画和动漫的切换 */}
+      <div className="Select">
+        {isCartoon == 1 ? (
+          <div className="common" onClick={() => ClicikType(2)}>
+            <div className="ContentIcon">
+              <SvgIcon name="CheckCircle" size={55} />
+            </div>
+            <div className="ContentTitle">
+              <span className="Comics">漫画</span>
+            </div>
+          </div>
+        ) : (
+          <div className="common" onClick={() => ClicikType(1)}>
+            <span className="Comics">漫画</span>
+          </div>
+        )}
 
-    return <ComicsWrapper>
-
-
-
+        {isCartoon == 2 ? (
+          <div className="common" onClick={() => ClicikType(1)}>
+            <div className="ContentIcon">
+              <SvgIcon name="CheckCircle" size={55} />
+            </div>
+            <div className="ContentTitle">
+              <span className="Comics">动漫</span>
+            </div>
+          </div>
+        ) : (
+          <div className="common" onClick={() => ClicikType(2)}>
+            <span className="Comics">动漫</span>
+          </div>
+        )}
+      </div>
+      {/* 轮播图 */}
+      <div className="carousel">
+        <Swiper className="Swiper" loop autoplay>
+          {colors.map((color, index) => (
+            <Swiper.Item key={index}>
+              <div className="Swiper" style={{ background: color }}>
+                {index + 1}
+              </div>
+            </Swiper.Item>
+          ))}
+        </Swiper>
+      </div>
     </ComicsWrapper>
+  );
+};
 
-
-}
-
-export default memo(Comics)
+export default memo(Comics);
--- src/component/Comics/styled.ts
+++ src/component/Comics/styled.ts
@@ -1,38 +1,68 @@
 import styled from "styled-components";
 
 const ComicsWrapper = styled.div`
-position: relative;
-height: 1000px;
-background-color: #F8F8F8;
-
-.optiy{
-    position:absolute;
-    width: 100%;
-    height: 40px;
-    background-color: rgba(255, 255, 255, 0.3);
-    z-index: 999;
-    left: 0;
-    top:0px;
+  overflow: hidden;
+  min-height: 100vh;
+  background: url(https://lanhu-dds-backend.oss-cn-beijing.aliyuncs.com/merge_image/imgs/2ec00cf1db6f4ed397b3167d105929b4_mergeImage.png)
+    100% no-repeat;
+  background-size: 100% 100%;
+  width: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  box-sizing: border-box;
+  position: relative;
+  .Select {
+    padding: 20px 20px;
     display: flex;
-    justify-content: space-around;
-    align-items: center;
-    .border{
-        width: 80px;
-        height: 40px;
-        display: flex;
-        justify-content: flex-end;
-        align-items: center;
-    }
-    .title{
-        color: black;
-        opacity: 1;
-        font-weight: 1000;
-    }
-    .title:hover{
-      color: #FD364E;
-    }
+    width: 100%;
+    height: 95px;
+    .common {
+      width: 60px;
+      height: 60px;
+      margin-right: 10px;
+      text-align: center;
+      line-height: 60px;
+      position: relative;
 
-}
+      .Comics {
+        font-size: 20px;
+        color: RGBA(121, 119, 120, 1);
+      }
+      .ContentTitle {
+        width: 100%;
+        height: 100%;
+        .Comics {
+          font-size: 30px;
+          font-weight: bolder;
+          color: black;
+          z-index: 9;
+        }
+      }
+      .ContentIcon {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 27px;
+        left: 0px;
+        z-index: 0;
+      }
+    }
+  }
 
+  .carousel {
+    border-radius: 16px;
+    position: relative;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 94%;
+    height: 200px;
+    overflow: hidden;
+
+    .Swiper {
+      width: 100%;
+      height: 100%;
+    }
+  }
 `;
-export default ComicsWrapper
+export default ComicsWrapper;
